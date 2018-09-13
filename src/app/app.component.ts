997c86700056f76cddd66ad84072985787ed5d9c
Mehedi Hasan <mehedi@mislbd.com>
2018-09-13 18:59:24 +0600

    material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'frontend';
+  messages = [];
+
+  constructor(private appService: AppService) {
+
+  }
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.appService.fetchData().subscribe(data => this.messages = data);
+  }
 }
