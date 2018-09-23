e8be3cd8cdd5585049cfe7fb09e9578f89814178
Mehedi Hasan <mehedi@mislbd.com>
2018-09-23 14:41:21 +0600

    login and register component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { AppService } from './app.service';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule } from '@angular/material';
+import { AppComponent } from "./app.component";
+import { AppService } from "./app.service";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatInputModule
+} from "@angular/material";
+import { RegisterComponent } from "./register/register.component";
+import { appRoutes } from "./app.routes";
+import { LoginComponent } from './login/login.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, RegisterComponent, LoginComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes),
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
