e27699c3d2225b95fb97ba2a30eab3fdd44c9a57
Mehedi Hasan <mehedi@mislbd.com>
2018-09-25 19:09:50 +0600

    path update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/ps-social-web'));
 
-app.get('/*', function (req, res) {
+app.get('/*all', function (req, res) {
 
     res.sendFile(path.join(__dirname + '/dist/ps-social-web/index.html'));
 });
